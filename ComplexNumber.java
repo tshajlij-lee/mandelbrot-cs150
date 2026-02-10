6a9f4ffc8898d8e88ab4a5f1dbbd3ec83fbd1314
Kevin-X. <Xiongkk32@gmail.com>
2026-02-10 13:30:54 -0600

    Small Update

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -1,5 +1,13 @@
-import java.io.IOException;
-
 public class ComplexNumber {
+
+    // Real Number
+    private double realNum;
+    // Imaginary Number
+    private double imagNum;
+
+    public ComplexNumber(double realNum, double imagNum) {
+        this.realNum = realNum;
+        this.imagNum = imagNum;
+    }
 
 }
